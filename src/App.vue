d98b4eb794985f9e7a72dd170be945b9ca3e7d3b
kanako-110 <kanako.110.ito@gmail.com>
2021-10-05 20:34:30 +0900

    :bulb: add
    todo comments

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,27 @@ import BookResult from './components/BookResult.vue';
 // TODO: type errで検索できなくなる
 // waringたち
 
+// skelton
+// https://codepen.io/abelhorihuela/pen/bZjrEW?css-preprocessor=scss
+// api fetch
+// https://v3.ja.vuejs.org/guide/composition-api-introduction.html#%E3%82%B9%E3%82%BF%E3%83%B3%E3%83%88%E3%82%99%E3%82%A2%E3%83%AD%E3%83%B3%E3%81%AA-computed-%E3%83%95%E3%82%9A%E3%83%AD%E3%83%8F%E3%82%9A%E3%83%86%E3%82%A3
+
+// data
+// https://developers.google.com/books/docs/v1/using
+
+// TODO: type errで検索できなくなる
+// waringたち
+// placeholder
+// overfetchしてないか
+// fetchDataの場所（Formだとemitしまくる。emitのemitの仕方
+
+// Todo: Appをpageとして、他に内容を置くのもあり
+// console.log消す
+// Sortボタンが、検索した後に出てきてもいいかも
+// pagination
+// totalNumberたちのわけかた・。propsでわたす方がよい、、
+// @app responseからbooksに変える
+
 export default defineComponent({
 	name: 'App',
 	components: { SortBox, Header, BookResult },
