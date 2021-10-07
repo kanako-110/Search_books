efe7272ded2dff85142fc8f18f5e85329c494cc9
kanako-110 <kanako.110.ito@gmail.com>
2021-10-07 15:15:49 +0900

    :heavy_plus_sign: add
    hennge pagination

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import VueLazyLoad from 'vue3-lazyload';
+import VPagination from '@hennge/vue3-pagination';
 
 createApp(App)
 	.use(VueLazyLoad)
+	.component('v-pagination', VPagination)
 	.mount('#app');
